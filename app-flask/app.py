0917d4f348265fe2faaff8507f55a7f1a61d5b1e
root <root@localhost.localdomain>
2019-09-15 22:21:43 +0100

    Done some more work with flask and jinja

--- app-flask/app.py
+++ app-flask/app.py
@@ -1,5 +1,8 @@
-from flask import Flask
+#!/usr/bin/python3
 
+import datetime
+from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,3 +11,13 @@ def index():
 @app.route("/adam")
 def adam():
     return "Hello madafaka!"
+@app.route("/<string:name>")
+def hello(name):
+    return "<h1>hello, %s<h1>" % (name)
+@app.route("/david")
+def index2():
+    trolololol = "trolololol"
+    now = datetime.datetime.now()
+    names = {"one": 1, "two": 2}
+    bday = now.month == 7 and now.day == 2
+    return render_template("table.html", headhead=trolololol, bday=bday, names=names)
