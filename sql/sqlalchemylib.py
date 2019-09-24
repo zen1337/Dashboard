b06d4c677b9f0933eb2eb543f1ad3ce09be62bca
root <root@localhost.localdomain>
2019-09-24 09:48:37 -0400

    changed some of the sql_py code for standard DB

--- sql/sqlalchemylib.py
+++ sql/sqlalchemylib.py
@@ -1,4 +1,5 @@
 import os
+import psycopg2
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -8,9 +9,9 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 def main():
-    flights = db.execute("SELECT name, status, team FROM statusp JOIN teams ON statusp.team_id = teams.id").fetchall() # fechall = run query and get all results return as list
-    for name in statusp:
-        print( "%s from %s is %s" % (statusp.name, statusp.team_id, statusp.status)) 
+    flights = db.execute("SELECT name, team_n, status FROM status JOIN team ON status.team_id = team.id").fetchall() # fechall = run query and get all results return as list
+    for name in flights:
+        print( "%s from %s is %s" % (status.name, status.team_id, status.status)) 
 
 if __name__ == "__main__":
     main()
